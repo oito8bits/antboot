f8e6345ef71a143e867363faa1dbd69296638461
Willian Oliveira <williandossantosdeoliveira287@gmail.com>
2024-11-02 20:03:13 -0300

    Improve printf()

--- libefi/console.c
+++ libefi/console.c
@@ -1,12 +1,5 @@
 #include <libefi.h>
 
-static VOID put(const CHAR16 *);
-static VOID puti(INTN);
-static INTN put_until_find_percent(const CHAR16 *);
-static INTN convert_to_string(va_list vl, const CHAR16 *);
-
-static VOID print_hex(CHAR16 *format, UINT64);
-
 static VOID put(const CHAR16 *string)
 {
   ST->ConOut->OutputString(ST->ConOut, (CHAR16 *) string);
@@ -19,40 +12,10 @@ static VOID puti(INTN number)
   put(string);
 }
 
-static INTN put_until_find_percent(const CHAR16 *str)
-{
-  CHAR16 *p = (CHAR16 *) str;
-  INTN result = -1;
-  while(*p)
-  {
-    if(*p == L'%')
-    {
-      result = p - str;
-      break;
-    }
-
-    putchar(*p++);
-  }
-
-  return result;
-}
-
-static VOID print_hex(CHAR16 *format, UINT64 number)
+static VOID print_hex(UINT64 number)
 {
   CHAR16 str[17] = {0};
   uitoh(str, number);
-  
-  while(*format != 'X' && *format != 'x')
-  {
-    switch(*format++)
-    {
-      case '#':
-        put(L"0x");
-        break;
-    }
-
-  }
-  
   put(str);
 }
 
@@ -69,42 +32,40 @@ VOID putchar(CHAR16 character)
   ST->ConOut->OutputString(ST->ConOut, string);
 }
 
-// Ainda falta terminar
 INTN printf(const CHAR16 *format, ...)
 {
   va_list vl;
 
   va_start(vl, format);
-
-  CHAR16 *p = (CHAR16 *) format, *q, c;
-  _Bool breakwhile = 0;
-  INTN offset = 0;
-  while((offset = put_until_find_percent(p)) != -1)
+  while(*format)
   {
-    q = p;
-    p += offset;
-    
-    while(*p && !breakwhile)
-    { 
-      c = *p++;
-      switch(c)
+    if(*format == L'%')
+    {
+      format++;
+      switch(*format)
       {
-        case 'i':
-          puti(va_arg(vl, int));
-          breakwhile = 1;
+        case L'%':
+          putchar(L'%');
           break;
-        case 'c':
-          putchar((CHAR16) va_arg(vl, unsigned int));
-          breakwhile = 1;
+        case L'i':
+          puti(va_arg(vl, INTN));
           break;
-        case 'x':
-          print_hex(q, va_arg(vl, UINT64));
-          breakwhile = 1;
+        case L'c':
+          putchar((CHAR16) va_arg(vl, UINTN));
+          break;
+        case L's':
+          put(va_arg(vl, CHAR16 *));
+          break;
+        case L'X':
+        case L'x':
+          print_hex(va_arg(vl, UINT64));
           break;
       }
     }
+    else
+      putchar(*format);
 
-    breakwhile = 0;
+    format++;
   }
 
   va_end(vl);
