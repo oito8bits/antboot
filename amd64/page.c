136248f946e69c92a1696f96ba8f390fcd3186b2
Willian Oliveira <williandossantosdeoliveira287@gmail.com>
2025-01-17 22:39:09 -0300

    Implement page_init() and page_map_pages()

--- amd64/page.c
+++ amd64/page.c
@@ -1,6 +1,8 @@
 #include <page.h>
 #include <cr3.h>
 
+UINT64 *pmd = 0;
+
 UINT64 page_get_p4d_idx(EFI_VIRTUAL_ADDRESS address)
 {
   return address >> 39 & 0x1ff;
@@ -21,7 +23,18 @@ UINT64 page_get_pte_idx(EFI_VIRTUAL_ADDRESS address)
   return address >> 12 & 0x1ff;
 }
 
-VOID page_map_pages(EFI_PHYSICAL_ADDRESS *address)
+VOID page_map_pages(EFI_PHYSICAL_ADDRESS address_base, UINTN npages)
 {
+  UINTN i;
+  for(i = 0; i < npages; i += PAGE_2MIB)
+    pmd[page_get_pmd_idx(address_base + i)] = address_base + i + 0x83;
+}
+
+VOID page_init(VOID)
+{
+  UINT64 *p4d = (UINT64 *) cr3_get();
+  UINT64 *pud = (UINT64 *) (p4d[0] & ~0xfff);
   
+  pmd = (UINT64 *) (pud[0] & ~0xfff);
+  printf(L"pmd %x, pud: %x, p4d: %x\r\n", pmd, pud, p4d[0]);
 }
--- include/amd64/page.h
+++ include/amd64/page.h
@@ -1,13 +1,15 @@
 #ifndef _PAGE_INCLUDE_
 #define _PAGE_INCLUDE_
 
-#include <efi.h>
+#include <libefi.h>
+#define PAGE_2MIB 2 << 20
 
 UINT64 page_get_p4d_idx(EFI_VIRTUAL_ADDRESS address);
 UINT64 page_get_pud_idx(EFI_VIRTUAL_ADDRESS address);
 UINT64 page_get_pmd_idx(EFI_VIRTUAL_ADDRESS address);
 UINT64 page_get_pte_idx(EFI_VIRTUAL_ADDRESS address);
 
-VOID page_map_pages(EFI_PHYSICAL_ADDRESS *);
+VOID page_map_pages(EFI_PHYSICAL_ADDRESS, UINTN);
+VOID page_init(VOID);
 
 #endif
